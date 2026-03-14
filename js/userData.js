578cd65c04e03225830d66c350ff13e60146d6d0
ilias <iliasfransen@hotmail.com>
2026-03-14 12:37:36 +0100

    update files alles

--- js/userData.js
+++ js/userData.js
@@ -1,8 +1,18 @@
 const user = localStorage.getItem("LoggedinUser");
+console.log(user);
 
 //tijdelijk zien wie er is ingelogd
 if (!user) {
     window.location.href = "/login.html";
 }
+
+
+const userData = JSON.parse(localStorage.getItem("LoggedinUser"));
+const usernameContainer = document.getElementById("username");
+const imgContainer = document.getElementById("profiel-picutre");
+
+
+usernameContainer.textContent = userData.username;
+imgContainer.src = userData.img;
 
 
