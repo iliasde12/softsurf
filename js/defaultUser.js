bb141409aea698623bcd407cb707d9c5ebd025bb
ilias <iliasfransen@hotmail.com>
2026-03-15 16:18:20 +0100

    default user

---
+++ js/defaultUser.js
@@ -0,0 +1,11 @@
+function saveUser() {
+    const user = {
+        id: Date.now(),
+        username: "test",
+        email: "test@test.com",
+        wachtwoord: "test12345",
+        avatar: "https://em-content.zobj.net/thumbs/240/apple/354/grinning-face_1f600.png",
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+}
