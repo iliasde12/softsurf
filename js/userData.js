714c115826edd5e72bacf130d43ba94dda2fcd24
ilias <iliasfransen@hotmail.com>
2026-03-15 15:55:59 +0100

    update tijdelijk uitzetten

--- js/userData.js
+++ js/userData.js
@@ -3,9 +3,9 @@ console.log(user);
 
 
 //tijdelijk zien wie er is ingelogd
-if (!user) {
+/*if (!user) {
     window.location.href = "/login.html";
-}
+}*/
 
 //de data showen en de nodige containers selecteren
 const userData = JSON.parse(localStorage.getItem("LoggedinUser"));
