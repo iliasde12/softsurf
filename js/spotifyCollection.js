e37c6d8b81396a66e4c6bc5a82da4c898367393e
ilias <iliasfransen@hotmail.com>
2026-03-16 10:35:37 +0100

    ben nog bezig met de collection

--- js/spotifyCollection.js
+++ js/spotifyCollection.js
@@ -1,5 +1,35 @@
-async function GetSpotifySongs() {
-    const token = localStorage.getItem("access_token_spotify");
-    
-    
+async function GetSpotifyPlaylistColection() {
+    const accessToken  = localStorage.getItem("access_token_spotify");
+    const idPlaylist = "1TFmBO87jfnxCNGUIVS9AM";
+
+     try {
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${idPlaylist}/items`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            }
+        });
+ 
+        //console.log(response.status);
+
+        if (!response.ok) {
+            throw new Error(`Spotify API error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+     
+    } catch (error) {
+        console.error("Error getting Spotify token:", error);
+        throw error;
+    }
+
+
+}
+
+GetSpotifyPlaylistColection();
+
+function CreateElementDom(){
+
 }
--- js/userData.js
+++ js/userData.js
@@ -1,18 +1,28 @@
-const user = localStorage.getItem("LoggedinUser");
-console.log(user);
+document.addEventListener("DOMContentLoaded", () => {
+    const user = localStorage.getItem("LoggedinUser");
+    //console.log(user);
 
-//tijdelijk zien wie er is ingelogd
-/*if (!user) {
-    window.location.href = "/login.html";
-}*/
+    //tijdelijk zien wie er is ingelogd
+    /*if (!user) {
+        window.location.href = "/login.html";
+    }*/
 
-//de data showen en de nodige containers selecteren
-const userData = JSON.parse(localStorage.getItem("LoggedinUser"));
-document.getElementById("username").innerText = userData.username;
-document.getElementById("profiel-picutre").src = userData.img;
 
-document.getElementById("uitloggen").addEventListener("click", () => {
-    localStorage.removeItem("LoggedinUser");
-    localStorage.removeItem("access_token_spotify");
-    window.location.href = "/login.html";
+    //containers
+    const userData = JSON.parse(localStorage.getItem("LoggedinUser"));
+    const username = document.getElementById("username");
+    const img = document.getElementById("profiel-picutre");
+    const uitloggen = document.getElementById("uitloggen");
+
+
+    //data vullen
+    username.innerText = userData.username;
+    img.src = userData.img;
+
+    uitloggen.addEventListener("click", () => {
+        localStorage.removeItem("LoggedinUser");
+        localStorage.removeItem("access_token_spotify");
+        window.location.href = "/login.html";
+    });
+
 });
