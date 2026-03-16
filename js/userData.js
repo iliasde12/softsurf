a7996b0d624235f702b265d5c5211e945532e8f1
ilias <iliasfransen@hotmail.com>
2026-03-16 17:14:50 +0100

    overall is de js file gevoegd en overal wordt de data dynamisch genomen uit de user object

--- js/userData.js
+++ js/userData.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const user = localStorage.getItem("LoggedinUser");
-    console.log(user);
-
+ const userData = JSON.parse(localStorage.getItem("LoggedinUser"));
     //tijdelijk zien wie er is ingelogd
     /*if (!user) {
         window.location.href = "/login.html";
     }*/
 
-    //de data showen en de nodige containers selecteren
-    const userData = JSON.parse(localStorage.getItem("LoggedinUser"));
-    document.getElementById("username").innerText = userData.username;
-    document.getElementById("profiel-picutre").src = userData.img;
 
-    document.getElementById("uitloggen").addEventListener("click", () => {
+    //containers
+   
+    const username = document.getElementById("username");
+    const img = document.getElementById("profiel-picutre");
+    const uitloggen = document.getElementById("uitloggen");
+
+
+    //data vullen
+    username.innerText = userData.username;
+    img.src = userData.img;
+
+    uitloggen.addEventListener("click", () => {
         localStorage.removeItem("LoggedinUser");
         localStorage.removeItem("access_token_spotify");
         window.location.href = "/login.html";
     });
+
 });
