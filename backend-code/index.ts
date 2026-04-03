8ed1c836d6093dd5943ede1ebf512de16ed5ed11
ilias <iliasfransen@hotmail.com>
2026-04-03 11:26:47 +0200

    test routest

--- backend-code/index.ts
+++ backend-code/index.ts
@@ -18,12 +18,16 @@ app.set("views", path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
+const testRoutes = require("./routes/test");
+
+app.use("/", testRoutes);
+
+/*app.get("/", (req, res) => {
   res.render("index", {
     title: "Hello World",
     message: "Hello World",
   });
-});
+});*/
 
 app.listen(app.get("port"), () => {
   console.log("Server started on http://localhost:" + app.get("port"));
