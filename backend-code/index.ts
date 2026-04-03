d768fd9f63610d31bf46abc3e9102305dc471929
cesaarlasure@student.ap.be <cesaarlasure249@gmail.com>
2026-04-03 10:49:42 +0200

    Mongo add

--- backend-code/index.ts
+++ backend-code/index.ts
@@ -1,26 +1,30 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
 
-const app : Express = express();
+const app: Express = express();
+const URI: string =
+  "mongodb+srv://SoftSurfUser:<db_password>@softsurf.pxq1kmm.mongodb.net/?appName=SoftSurf";
+const client: MongoClient = new MongoClient(URI);
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        title: "Hello World",
-        message: "Hello World"
-    })
+  res.render("index", {
+    title: "Hello World",
+    message: "Hello World",
+  });
 });
 
 app.listen(app.get("port"), () => {
-    console.log("Server started on http://localhost:" + app.get('port'));
+  console.log("Server started on http://localhost:" + app.get("port"));
 });
