33fc7c3963fd85eaef48e9828d6993d98306968a
cesaarlasure@student.ap.be <cesaarlasure249@gmail.com>
2026-04-03 11:24:00 +0200

    routes template/partials

---
+++ backend-code/routes/test.ts
@@ -0,0 +1,10 @@
+import express, { Express } from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Hello World",
+    message: "Hello World",
+  });
+});
