ab462205ce7b89403df8dd3a6766037e5414590a
ilias <iliasfransen@hotmail.com>
2026-04-03 11:31:00 +0200

    update

--- backend-code/index.ts
+++ backend-code/index.ts
@@ -6,6 +6,7 @@ import { MongoClient } from "mongodb";
 dotenv.config();
 
 const app: Express = express();
+//later moet dit naar env voor securty
 const URI: string =
   "mongodb+srv://SoftSurfUser:softsurf990304@softsurf.pxq1kmm.mongodb.net/?appName=SoftSurf";
 const client: MongoClient = new MongoClient(URI);
