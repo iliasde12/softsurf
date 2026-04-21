58290c9cef58ac1203d7445d76ef950f7d832956
ilias <iliasfransen@hotmail.com>
2026-04-21 16:58:21 +0200

    update

--- backend-code/databse.ts
+++ backend-code/databse.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt-ts";
 
 dotenv.config();
 
@@ -9,7 +9,41 @@ const URI: string =
   "mongodb+srv://SoftSurfUser:softsurf990304@softsurf.pxq1kmm.mongodb.net/?appName=SoftSurf";
 const client: MongoClient = new MongoClient(URI);
 
+/*async function main() {
+  try {
+    await client.connect();
+
+   
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}*/
+let isConnected = false;
+
+export async function connectDB() {
+  if (!isConnected) {
+    await client.connect();
+    isConnected = true;
+  }
+  return client.db("softsurf");
+}
+
 //create user
-export async function CreateUser(username: string,email: string,password: string,) {}
+export async function CreateUser(
+  username: string,
+  email: string,
+  password: string,
+) {
+  const db = await connectDB();
+  const users = db.collection("users");
+
+  return await users.insertOne({
+    username,
+    email,
+    password,
+  });
+}
 
 //update user
