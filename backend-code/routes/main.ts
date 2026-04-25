77eeddfeea0881bde469dfdb50d2cb20e3b4c749
ilias <iliasfransen@hotmail.com>
2026-04-25 20:45:42 +0200

    update

--- backend-code/routes/main.ts
+++ backend-code/routes/main.ts
@@ -1,4 +1,7 @@
 import express, { Router } from "express";
+import { User } from "../interfaces"
+import { login } from "../database"
+ 
 
 const router: Router = express.Router();
 
@@ -9,6 +12,22 @@ router.get("/", (req, res) => {
 
 router.get("/login", (req, res) => {
   res.render("login");
+});
+
+router.post("/login", async(req, res) => {
+    const email : string = req.body.email;
+    const password : string = req.body.password;
+    try {
+      //returnd een user
+        let user : User = await login(email, password);
+        //delete de password uit de object
+        delete user.password; 
+        //zet de rest in de session 
+        req.session.user = user;
+        res.redirect("/")
+    } catch (e : any) {
+        res.redirect("/login");
+    }
 });
 
 router.get("/register", (req, res) => {
