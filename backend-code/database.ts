dbef6e7fcec3e4a57d187ff7fb275031976d907d
ilias <iliasfransen@hotmail.com>
2026-04-28 14:43:45 +0200

    update

--- backend-code/database.ts
+++ backend-code/database.ts
@@ -19,6 +19,9 @@ export const MONGODB_URI: string =
   "mongodb+srv://SoftSurfUser:softsurf990304@softsurf.pxq1kmm.mongodb.net/?appName=SoftSurf";
 const client: MongoClient = new MongoClient(MONGODB_URI);
 
+//db connection
+export const db = client.db("softsurf");
+
 //exit function
 async function exit() {
   try {
@@ -38,7 +41,7 @@ export async function connect() {
 }
 
 //users
-export const userCollection = client.db("softsurf").collection<User>("users");
+export const userCollection = db.collection<User>("users");
 
 // create users
 export async function createUser(
@@ -47,7 +50,6 @@ export async function createUser(
   password: string,
   avatar: { url: string; alt: string },
 ) {
-
   //als de email al bestaat
   const existingUser = await userCollection.findOne({ email });
   if (existingUser) {
@@ -64,7 +66,7 @@ export async function createUser(
       updatedAt: now,
     });
   } catch (e) {
-      throw new Error("error: " + e);
+    throw new Error("error: " + e);
   }
 }
 
@@ -73,29 +75,25 @@ export async function editUser(
   username: string,
   email: string,
   password: string,
-  avatar: { url: string; alt: string }){
-
-    
-
-
-}
+  avatar: { url: string; alt: string },
+) {}
 
 // login user
 export async function login(email: string, password: string): Promise<User> {
-    if (email === "" || password === "") {
-        throw new Error("Email and password required");
-    }
+  if (email === "" || password === "") {
+    throw new Error("Email and password required");
+  }
 
-    const user = await userCollection.findOne<User>({ email });
+  const user = await userCollection.findOne<User>({ email });
 
-    if (!user) {
-        throw new Error("User not found");
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    const passwordMatch = await bcrypt.compare(password, user.password!);
-    if (!passwordMatch) {
-        throw new Error("Password incorrect");
-    }
+  const passwordMatch = await bcrypt.compare(password, user.password!);
+  if (!passwordMatch) {
+    throw new Error("Password incorrect");
+  }
 
-    return user;
+  return user;
 }
